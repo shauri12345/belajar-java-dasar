4e5b754f74b29e04bad10433a8c8862ce316c39f
java <shauriramadhan01@gmail.com>
2023-12-16 09:32:58 +0700

    variable

---
+++ src/com/tutorial/VariableAssigment.java
@@ -0,0 +1,14 @@
+package com.tutorial;
+
+public class VariableAssigment {
+    public static void main(String[] args) {
+        int a = 20;
+        System.out.println("nilai a = " + a);
+
+        //deklarasi
+        int b;
+        b = 23;
+        System.out.println("nilai b = " + b);
+
+    }
+}
